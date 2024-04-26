89e7c8ae2286f88d63c8ca7881595a35e393082b
yuyuesun <133299273+yuyuesun@users.noreply.github.com>
2024-04-26 15:46:20 -0400

    Update process_nonrelational.py

--- process_nonrelational.py
+++ process_nonrelational.py
@@ -1,11 +1,3 @@
-"""
-File : /scripts/process_nonrelational.py
-
-Description:
-1. Create a MongoDB database with desired fields
-2. Push data into our MongoDB database
-"""
-
 from utils import connect_to_mongodb  # Assuming connMongoDB is your MongoDB connection function
 import json
 import time 
@@ -52,10 +44,12 @@ def insert_data_from_file(file_path, db_name, collection_name, timeout_seconds=3
     
     start_time = time.time()
     doc_count = 0
-    upserts = 0
+    duplicates = 0
     
     with open(file_path, 'r') as file:
         for line_number, line in enumerate(file, start=1):
+            if line_number <= 76000:
+                continue
             if time.time() - start_time > timeout_seconds:
                 print("Reached timeout threshold.")
                 break
@@ -64,26 +58,27 @@ def insert_data_from_file(file_path, db_name, collection_name, timeout_seconds=3
                     continue
                 document = json.loads(line)
                 processed_document = process_document(document)
-                result = collection.update_one({'id_str': processed_document['id_str']}, {'$set': processed_document}, upsert=True)
-                if result.upserted_id is not None:
-                    upserts += 1
+                collection.insert_one(processed_document)
                 doc_count += 1
                 if doc_count % 1000 == 0:
-                    print(f'\t Total Documents Inserted or Updated: {doc_count}')
+                    print(f'\t Total Documents Inserted: {doc_count}')
+            except pymongo.errors.DuplicateKeyError:
+                duplicates += 1  # Track duplicate insert attempts
             except json.JSONDecodeError as e:
                 print(f'Error parsing JSON at line {line_number}: {e}')
                 print(f'Offending line content: "{line.strip()}"')
             except Exception as e:
                 print(f'\t Document Insert Unsuccessful for doc_count: {doc_count} as {e}')
 
-    if upserts:
-        print(f"Documents inserted or updated: {upserts}.")
+    if duplicates:
+        print(f"Skipped {duplicates} duplicates.")
+
 
 if __name__ == "__main__":
     file_path = 'corona-out-3'
     db_name = 'twitter'
     collection_name = 'nonrelational'
-    timeout_seconds = 60
+    timeout_seconds = 3600
 
     print('MongoDB: *** Data Insertion Started ***')
     insert_data_from_file(file_path, db_name, collection_name, timeout_seconds)
