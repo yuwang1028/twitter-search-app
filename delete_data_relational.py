6a25d4cae4ddcc87b0b9b6fe088b134c4a7ff8ce
yuyuesun <133299273+yuyuesun@users.noreply.github.com>
2024-04-26 11:04:55 -0400

    Update delete_data_relational.py

--- delete_data_relational.py
+++ delete_data_relational.py
@@ -22,10 +22,33 @@ def delete_all_data(table_name):
             connection.close()
             print("MySQL connection is closed.")
 
+
+def drop_table(table_name):
+    connection = connect_to_mysql()
+    if connection is None:
+        print("MySQL connection failed.")
+        return
+
+    try:
+        cursor = connection.cursor()
+        drop_query = f"DROP TABLE IF EXISTS {table_name}"
+        cursor.execute(drop_query)
+        connection.commit()  # Make sure to commit to apply the changes
+        print(f"Table '{table_name}' has been dropped.")
+    except Error as e:
+        print(f"Error while trying to drop the table: {e}")
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed.")
+
 if __name__ == "__main__":
-    # table_name = 'user_profile'  # Specify the name of the table from which to delete data
+    table_name = 'user_profile'  # Specify the name of the table from which to delete data
     # table_name = 'tweets'
     # table_name = 'reply'
     # table_name = 'quoted_tweets'
-    table_name = 'retweets'
-    delete_all_data(table_name)
+    # table_name = 'retweets'
+    # table_name = 'logs'
+    # delete_all_data(table_name)
+    drop_table(table_name)
